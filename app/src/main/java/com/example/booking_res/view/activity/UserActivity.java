34ed1e1e4aa7afc3f9ca3d872d23d414fc78a2b6
an.phamky1234@gmail.com <an.phamky1234@gmail.com>
2024-08-03 13:37:44 +0700

    update 3 ( 08/02/2024 )

--- app/src/main/java/com/example/booking_res/view/activity/UserActivity.java
+++ app/src/main/java/com/example/booking_res/view/activity/UserActivity.java
@@ -1,6 +1,9 @@
 package com.example.booking_res.view.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,14 +12,30 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.booking_res.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class UserActivity extends AppCompatActivity {
-
+    private FirebaseAuth mAuth;
+    private Button btn_logout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_user);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        btn_logout = findViewById(R.id.btn_logout);
+
+        btn_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth.signOut();
+                startActivity(new Intent(UserActivity.this, MainActivity.class));
+                finish();
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
